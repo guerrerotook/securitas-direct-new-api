2c7acf19a92f1d178bcae2a4b78ff3c4c256c47f
Clinton Gormley <clintongormley@gmail.com>
2026-08-28 14:06:45 +0200

    chore: pre-bump to 5.8.0-beta.2 + finalise v5.8.0 changelog (#577)
    Bump manifest + card ?v= cache-bust tokens 5.8.0-beta.1 -> 5.8.0-beta.2,
    and finalise the v5.8.0 changelog for the beta.2 release notes: add the
    Fixed entry for the cleared-mapping bug (#575) and note it in the summary.
    Rebased onto current main (includes #573/#575/#576/#578).

--- custom_components/securitas/www/verisure-owa-alarm-chip.js
+++ custom_components/securitas/www/verisure-owa-alarm-chip.js
@@ -13,7 +13,7 @@ import {
   defaultArmState,
   attachGesture,
   _makeLegacyShim,
-} from "./verisure-owa-alarm-shared.js?v=5.8.0-beta.1";
+} from "./verisure-owa-alarm-shared.js?v=5.8.0-beta.2";
 
 class VerisureOwaAlarmBadge extends HTMLElement {
   constructor() {
